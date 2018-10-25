18a93e6955b5875e6d872d8feee3228994c14986
asm <asm>
2018-10-25 21:42:21 +0300

    per

---
+++ per/src/main/webapp/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BaseBarchartModule } from './barchart/barchart.module';
+import { BaseDoughnutchartModule } from './doughnutchart/doughnutchart.module';
+import { BaseLinechartModule } from './linechart/linechart.module';
+import { BasePiechartModule } from './piechart/piechart.module';
+import { BasePolarareachartModule } from './polarareachart/polarareachart.module';
+import { BaseRadarchartModule } from './radarchart/radarchart.module';
+
+@NgModule({
+    imports: [
+        BaseBarchartModule,
+        BaseDoughnutchartModule,
+        BaseLinechartModule,
+        BasePiechartModule,
+        BasePolarareachartModule,
+        BaseRadarchartModule
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BaseDashboardModule {}
